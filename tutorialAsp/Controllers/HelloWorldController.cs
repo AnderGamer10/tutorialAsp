f8dfb250b35454e9d6c591bf293180d05728d799
AnderGamer10 <anderedondo10@hotmail.com>
2021-11-17 12:37:17 +0100

    Agregar archivos de proyecto.

---
+++ tutorialAsp/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace tutorialAsp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+         
+        // /HelloWorld/Welcome?name=Rick&numtimes=10 ponemos esto para buscar y darles valores
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+        public string Saludos(int? id)
+        {
+            return "Hola... de nuevo "+id;
+        }
+    }
+}
---
+++ tutorialAsp/Data/tutorialAspContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using tutorialAsp.Models;
+
+namespace tutorialAsp.Data
+{
+    public class tutorialAspContext : DbContext
+    {
+        public tutorialAspContext (DbContextOptions<tutorialAspContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<tutorialAsp.Models.Movie> Movie { get; set; }
+    }
+}
